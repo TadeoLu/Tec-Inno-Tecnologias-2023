edfc0c482b4d9e2ce22d7c515abda5247ebad0fa
tadeolu <alumno23.centrone.tadeo@ipm.edu.ar>
2023-04-26 10:31:46 -0300

    Carrusel2

--- Tec. Inno. Tecnologica 2023/Carousel 2/carrusel.js
+++ Tec. Inno. Tecnologica 2023/Carousel 2/carrusel.js
@@ -1,26 +1,27 @@
 function activarMiCarousel(id, delay){
     let carrousel = document.getElementById(id);
+    carrousel.getElementsByTagName("img")[0].style.display = "block";
     for(var i = 1; i < carrousel.getElementsByTagName("img").length; i++){
         var img = carrousel.getElementsByTagName("img")[i];
         img.style.display = "none";
     }
-    setInterval(cambiarImagen(id),3000);
+    setInterval(cambiarImagen,delay,id);
 }
 
 function cambiarImagen(id){
-    console.log("Tamo ruleta");
     let carrousel = document.getElementById(id);
-    let lenght = carrousel.getElementsByTagName("img").length;
+    let length = carrousel.getElementsByTagName("img").length;
     for(var i = 0; i < length; i++){
         var img = carrousel.getElementsByTagName("img")[i];
         if(i > 0){
-            var imgAnterior = carrousel.getElementsByTagName("img")[i-1];
+            var imgAnterior = carrousel.getElementsByTagName("img")[(i-1)];
         }else{
             var imgAnterior = carrousel.getElementsByTagName("img")[length-1];
         }
         if(imgAnterior.style.display == "block"){
             imgAnterior.style.display = "none";
             img.style.display = "block";
+            break;
         }
     }
 }
